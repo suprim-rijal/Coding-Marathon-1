64a479bceee68e04d41a95da86ad33e80aff13be
suprim-rijal <suprimrijal1@gmail.com>
2026-09-09 23:56:56 +0300

    Add RecipeManager component and self-assessment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,11 @@
 function Home() {
   return (
     <div>
-      <h2>Group # ?</h2>
+      <h2>Group 7</h2>
+
       <p>Code Marathon 1</p>
     </div>
   );
 }
 
-export default Home; 
+export default Home;
